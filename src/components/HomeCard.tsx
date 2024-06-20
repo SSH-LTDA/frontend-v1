b27f9fab8c204a1c6d6b196c8faf52eeb1326b44
unknown <igorluizbelterrosiak@gmail.com>
2024-06-20 13:54:52 -0300

    fix homeCard width

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -11,7 +11,7 @@ interface HomeCardProps {
 const HomeCard: React.FC<HomeCardProps> = ({ title, description, image, isReverse }) => {
   return (
     <Row
-      className={`w-[85vw] mx-auto my-20 flex justify-between border-[#14274A] max-[768px]:w-[95vw] ${isReverse ? "border-l-2 pl-6" : "flex-row-reverse border-r-2 pr-6 border-[#14274A]"} `}
+      className={`max-w-[85vw] mx-auto my-20 flex justify-between border-[#14274A] max-[768px]:w-[95vw] ${isReverse ? "border-l-2 pl-6" : "flex-row-reverse border-r-2 pr-6"}`}
     >
       <div className="w-[48%] my-auto">
         <h3 className="text-5xl font-bold mb-2">{title}</h3>
