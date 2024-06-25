a4c002b7718d1aa132effacbe291b9e1c5bc5159
Onipotente <mello.matheus3012@gmail.com>
2024-06-25 13:53:39 -0300

    :sparkles: feat: Add Register, Login and UserInfo Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-
-import Home from "./pages/Home";
-import ReservationForm from "./components/ReservationForm/ReservationForm";
-import Rooms from "./pages/Rooms";
-import Facilities from "./pages/Facilities";
-import Contact from "./pages/Contact";
-import HomeContent from "./pages/Home/HomeContent";
-import PaymentForm from "./components/PaymentForm";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ReservationForm from './components/ReservationForm/ReservationForm';
+import Rooms from './pages/Rooms';
+import Facilities from './pages/Facilities';
+import Contact from './pages/Contact';
+import HomeContent from './pages/Home/HomeContent';
+import PaymentForm from './components/PaymentForm';
+import RegisterForm from './pages/auth/register';
+import UserInfo from './pages/user/UserProfile';
+import LoginPage from './pages/auth/login';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="" element={<HomeContent />} />
-          <Route path="facilities" element={<Facilities />} />
-          <Route path="rooms" element={<Rooms />} />
-          <Route path="contact-us" element={<Contact />} />
-        </Route>
-        <Route path="/reservation-form" element={<ReservationForm />} />
-        <Route path="/payment" element={<PaymentForm />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="" element={<HomeContent />} />
+        <Route path="facilities" element={<Facilities />} />
+        <Route path="rooms" element={<Rooms />} />
+        <Route path="contact-us" element={<Contact />} />
+      </Route>
+      <Route path="/reservation-form" element={<ReservationForm />} />
+      <Route path="/payment" element={<PaymentForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/user-info" element={<UserInfo />} />
+    </Routes>
   );
 }
 
 export default App;
+
+
 
 
 /* 
