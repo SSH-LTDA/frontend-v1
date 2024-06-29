d0a58bc82803fe3e7874fd99d12c3effd244c8de
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:34:35 -0300

    feat: Developing AuthContext.tsx

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useState, useEffect, ReactNode, useContext, useCallback } from "react";
+import { User } from "../types/User.ts";
+
+interface AuthContextType {
+  user: User | null;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const logout = useCallback(() => {
+    localStorage.removeItem("user");
+    setUser(null);
+  }, []);
+
+  return <AuthContext.Provider value={{ user, logout }}>{children}</AuthContext.Provider>;
+};
+
+const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export { AuthProvider, useAuth };
