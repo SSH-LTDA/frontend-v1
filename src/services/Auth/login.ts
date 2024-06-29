b86be217d1043791bd8c5418e8cd603c7334b062
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:35:27 -0300

    feat: Developing login.ts service file

---
+++ src/services/Auth/login.ts
@@ -0,0 +1,13 @@
+import { request } from "../../helpers/request.ts";
+import { User } from "../../types/User.ts";
+
+type loginData = {
+  email: string;
+  password: string;
+};
+
+async function login(data: loginData): Promise<User> {
+  return request({ method: "POST", url: "auth/login", data });
+}
+
+export default login;
