22aad6e4381fa0981ecc8e77423e11c05000f9df
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:36:28 -0300

    feat: Adding AuthProvider & Toast Container and Css to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <ToastContainer />
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
