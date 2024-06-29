f6721127825563b47bf17cd19447e5e8e006961a
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-06-29 15:36:47 -0300

    feat: Developing register.ts service file

---
+++ src/services/Auth/register.ts
@@ -0,0 +1,16 @@
+import { User } from "../../types/User.ts";
+import { request } from "../../helpers/request.ts";
+
+type registerData = {
+  name: string;
+  cpf: string;
+  email: string;
+  phone: string;
+  password: string;
+};
+
+async function register(data: registerData): Promise<User> {
+  return request({ method: "POST", url: "auth/register", data });
+}
+
+export default register;
