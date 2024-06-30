e00a234448d96d06488b6c91263532beb94e5538
unknown <igorluizbelterrosiak@gmail.com>
2024-06-30 13:30:49 -0300

    :range: fix: fix type of accommodation

--- src/types/Accommodation.ts
+++ src/types/Accommodation.ts
@@ -1,8 +1,10 @@
 export interface Accommodation {
   id: string;
   type: string;
-  size: number;
+  beds: number;
+  price: number;
   photos: string[];
+  description: string;
   facilities: string[];
   guestCapacity: number;
 }
